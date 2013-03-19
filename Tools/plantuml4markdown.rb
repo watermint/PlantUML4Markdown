4e0b9fe713393a4229134150fe3b0e91da5c9133
Takayuki Okazaki <takayuki.okazaki@gmail.com>
2013-03-20 07:30:12 +0900

    plantuml convert + html convert

--- Tools/plantuml4markdown.rb
+++ Tools/plantuml4markdown.rb
@@ -19,11 +19,58 @@ class PlantUML4Markdown
   end
 
   def initialize(path)
-    @path = path
+    @path     = path
+    @dirname  = File.dirname path
+    @basename = (File.basename path).sub(/\..+?$/, '')
   end
 
   def generate
+    uml       = {}
+    uml_count = 1
+    converted = []
     
+    open @path, 'r' do |f|
+      while line = f.gets
+        line.chomp!
+        if line.start_with?('@startuml') then
+          converted << "![Fig. #{uml_count}](#{@basename}-#{uml_count}.svg)"
+          u = [line]
+          while uml_line = f.gets
+            u << uml_line
+            break if uml_line.start_with?('@enduml')
+          end
+          uml[uml_count] = u
+          uml_count += 1
+        else
+          converted << line
+        end
+      end
+    end
+
+    open @path, 'w' do |f|
+      f.puts converted.join("\n")
+    end
+
+    open "#{@dirname}/#{@basename}.html", 'w' do |f|
+      f.puts <<"HTML"
+    <!DOCTYPE html>
+<html>
+<title>#{@basename}</title>
+
+<xmp theme="readable" style="display:none;">
+#{converted.join("\n")}
+</xmp>
+
+<script src="http://strapdownjs.com/v/0.2/strapdown.js"></script>
+</html>
+HTML
+    end
+
+    uml.each_pair do |index, body|
+      open "#{@dirname}/#{@basename}-#{index}.plantuml", "w" do |f|
+        f.puts body.join("\n")
+      end
+    end
   end
 end
 
